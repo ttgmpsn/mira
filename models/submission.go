e492740226a44ba5293f5ecf7a0f8444ed552a59
Michael <ttgmpsn@layer7.solutions>
2020-04-09 19:31:52 +0200

    ... forget to commit file deletions

--- models/submission.go
+++
@@ -1,5 +0,0 @@
-package models
-
-func (s *Submission) GetID() string           { return s.JSON.Data.Name }
-func (s *Submission) GetDraftsCount() float64 { return s.JSON.Data.DraftsCount }
-func (s *Submission) GetURL() string          { return s.JSON.Data.URL }
